61af8811323dc0bbc4448297c8b6810ca98e709f
janis-jenny <pcardenasvm@gmail.com>
2020-10-13 17:43:37 -0500

    computer choices, moves and add check winner

--- bin/main.rb
+++ bin/main.rb
@@ -2,7 +2,121 @@
 
 choices = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
-print 'What is your name? '
+def player_Choice(choices, player_name)
+  display_Board(choices)
+  puts 'Please make a move, choose a number: '
+  player_move = gets.chomp 
+  player_move = player_move.to_i
+  player_move -= 1
+  choices = check_Move(player_move, choices, player_name)
+  choices
+end
+
+def display_Board(choices)
+  puts " #{choices[0]}  | #{choices[1]}  | #{choices[2]}  "
+  puts '-----------'
+  puts " #{choices[3]}  | #{choices[4]}  | #{choices[5]}  "
+  puts '-----------'
+  puts " #{choices[6]}  | #{choices[7]}  | #{choices[8]}  "
+end
+
+def check_Move(player_move, choices, player_name)
+  if choices[player_move] === 'X' && choices[player_move] === 'O'
+    puts 'Valid move!'
+    choices[player_move] = 'X'
+    check_Winner(choices, player_name)
+    choices
+  else
+    display_Board(choices)
+    puts 'Invalid move! Choose another number: '
+    player_move = gets.chomp
+    check_Move(player_move, choices, player_name)
+  end
+end
+
+def computer_Choice(choices)
+  computer_choice = rand(1..9)
+  computer_choice -= 1
+  check_computer_Move(computer_choice, choices, player_name)
+end
+
+def check_computer_Move(computer_move, choices, player_name)
+  if choices[computer_move] === 'X' && choices[computer_move] === 'O'
+    choices[computer_move] = 'O'
+    check_Winner(choices, player_name)
+    choices
+  else
+    computer_move = rand(1..9)
+    computer_move -= 1
+    check_computer_Move(computer_move, choices, player_name)
+  end
+end
+
+def check_Winner(_choices, player_name)
+  winner = false
+  if array[0] == 'X' && array[1] == 'X' && array[2] == 'X'
+    winner = true
+    announce_Winner(player_name)
+  elsif array[3] == 'X' && array[4] == 'X' && array[5] == 'X'
+    winner = true
+    announce_Winner(player_name)
+  elsif array[6] == 'X' && array[7] == 'X' && array[8] == 'X'
+    winner = true
+    announce_Winner(player_name)
+  elsif array[0] == 'X' && array[3] == 'X' && array[6] == 'X'
+    winner = true
+    announce_Winner(player_name)
+  elsif array[0] == 'X' && array[4] == 'X' && array[8] == 'X'
+    winner = true
+    announce_Winner(player_name)
+  elsif array[1] == 'X' && array[4] == 'X' && array[7] == 'X'
+    winner = true
+    announce_Winner(player_name)
+  elsif array[2] == 'X' && array[5] == 'X' && array[8] == 'X'
+    winner = true
+    announce_Winner(player_name)
+  elsif array[2] == 'X' && array[4] == 'X' && array[6] == 'X'
+    winner = true
+    announce_Winner(player_name)
+  else
+    if array[0] == 'O' && array[1] == 'O' && array[2] == 'O'
+      announce_Loser(player_name)
+    elsif array[3] == 'O' && array[4] == 'O' && array[5] == 'O'
+      announce_Loser(player_name)
+    elsif array[6] == 'O' && array[7] == 'O' && array[8] == 'O'
+      announce_Loser(player_name)
+    elsif array[0] == 'O' && array[3] == 'O' && array[6] == 'O'
+      announce_Loser(player_name)
+    elsif array[0] == 'O' && array[4] == 'O' && array[8] == 'O'
+      announce_Loser(player_name)
+    elsif array[1] == 'O' && array[4] == 'O' && array[7] == 'O'
+      announce_Loser(player_name)
+    elsif array[2] == 'O' && array[5] == 'O' && array[8] == 'O'
+      announce_Loser(player_name)
+    elsif array[2] == 'O' && array[4] == 'O' && array[6] == 'O'
+      announce_Loser(player_name)
+    else
+      winner = false
+      winner
+    end
+  end
+end
+
+def announce_Winner(player_name)
+  puts "Congratulations #{player_name}! You are the winner!"
+end
+
+def announce_Loser(player_name)
+  puts "#{player_name} you lost!"
+end
+
+def game_Engine(choices, player_name)
+  current_choices = choices
+  current_choices = player_Choice(current_choices, player_name)
+  current_choices = computer_Choice(current_choices, player_name)
+end
+
+print 'What is your name?'
 player_name = gets.chomp
 puts "Welcome #{player_name}!"
 print 'Is it your first time playing tic-tac-toe? [Y]es [N]o? '
@@ -17,83 +131,8 @@ if tutorial == 'Y'
   puts 'When all 9 squares are full, the game is over.'
   puts 'If no player has 3 marks in a row, the game ends in a tie.'
   puts "Alright! Now you are able to continue, let's start the game!"
-  game_Engine(choices)
+  game_Engine(choices, player_name)
 else
   puts "Nice, let's start the game!"
-  game_Engine(choices)
-end
-
-def game_Engine(choices)
-  current_choices = choices
-  current_choices = player_Choice(current_choices)
-  current_choices = computer_Choice(current_choices)
-end
-
-def display_Board(choices)
-  puts " #{choices[0]}  | #{choices[1]}  | #{choices[2]}  "
-  puts '-----------'
-  puts " #{choices[3]}  | #{choices[4]}  | #{choices[5]}  "
-  puts '-----------'
-  puts " #{choices[6]}  | #{choices[7]}  | #{choices[8]}  "
-end
-
-def player_Choice(choices)
-  display_Board(choices)
-  puts 'Please make a move, choose a number: '
-  player_move = gets.chomp
-  player_move -= 1
-  choices = check_Move(player_move, choices)
-  return choices
-end
-
-def computer_Choice(choices)
-end
-
-def check_Move(player_move, choices)
-    if choices[player_move] === 'X' || choices[player_move] === 'O'
-      puts 'Valid move!'
-      choices[player_move] = 'X'
-      check_Winner(choices, player_name)
-      return choices
-    else
-      display_Board(choices)
-      puts 'Invalid move! Choose another number: '
-      player_move = gets.chomp
-      check_Move(player_move, choices)
-    end
-end
-
-def check_Winner(choices, player_name)
-  winner = false
-  if array[0] == 'X' && array[1] == 'X' && array[2] =='X'
-    winner = true
-    announce_Winner(player_name)
-  elsif array[3] == 'X' && array[4] == 'X' && array[5] =='X'
-    winner = true
-    announce_Winner(player_name)
-  elsif array[6] == 'X' && array[7] == 'X' && array[8] =='X'
-    winner = true
-    announce_Winner(player_name)
-  elsif array[0] == 'X' && array[3] == 'X' && array[6] =='X'
-    winner = true
-    announce_Winner(player_name)
-  elsif array[0] == 'X' && array[4] == 'X' && array[8] =='X'
-    winner = true
-    announce_Winner(player_name)
-  elsif array[1] == 'X' && array[4] == 'X' && array[7] =='X'
-    winner = true
-    announce_Winner(player_name)
-  elsif array[2] == 'X' && array[5] == 'X' && array[8] =='X'
-    winner = true
-    announce_Winner(player_name)
-  elsif array[2] == 'X' && array[4] == 'X' && array[6] =='X'
-    winner = true
-    announce_Winner(player_name)
-  else
-    return winner
-  end
-end
-
-def announce_Winner(player_name)
-  puts "Congratulations #{player_name}! You are the winner!"
+  game_Engine(choices, player_name)
 end
